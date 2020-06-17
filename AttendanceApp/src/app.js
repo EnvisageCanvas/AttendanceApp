04e846b1f49d56dcd5ffcf4505b1d57c318eedf5
Shivam Shrivastava <chessfreakshivam@gmail.com>
2020-06-17 22:46:02 +0530

    Boilerplate

---
+++ AttendanceApp/src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+//Render the template onto the div 
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById('root'));    
